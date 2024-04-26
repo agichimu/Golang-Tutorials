a6d1c914563d5b7cf99858d92a345d0e26efa206
agichimu <alexandergichimu@zetech.ac.ke>
2024-04-26 14:59:10 +0300

    add of insurance function

--- boolean.go
+++
@@ -1,9 +0,0 @@
-package main
-
-/*import "fmt"
-
-func main() {
-	insuranceIncluded := false
-	fmt.Println("Insurance Included = ", insuranceIncluded)
-}
-*/
--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
 	_ "strings"
 )
 
 func main() {
+	Insurance()
 	String()
 
 	/*Variables are used to Store data*/
 
 	var nameOne string = "Alexander"
 	var agency string = "fast track!"
-	var totalDevelopers int = 29
-	carBrand := "Porsche."
+
+	//var totalDevelopers int = 29
+
+	//carBrand := "Porsche."
+
 	startingPrice := 50.55 /*Float*/
 
 	/*
@@ -30,13 +33,20 @@ func main() {
 
 	fmt.Println("Hello", nameOne)
 	fmt.Println(agency)
-	fmt.Println("My dream car is a", carBrand)
-	fmt.Println("My team consists of", totalDevelopers, "Developers.")
+	//fmt.Println("My dream car is a", carBrand)
+
+	//fmt.Println("My team consists of", totalDevelopers, "Developers.")
+
 	fmt.Printf("Our Prices range From %v", startingPrice)
 
 	/*
 		%v holds place of a variable in a formatted string
 	*/
+}
+
+func Insurance() {
+	insuranceIncluded := false
+	fmt.Println("Insurance Included = ", insuranceIncluded)
 }
 
 func String() {
@@ -46,6 +56,12 @@ func String() {
 	stg := "Hello, World!"
 	length := len(stg)
 	fmt.Println("Length =", length)
-	fmt.Println(strings.ToUpper(stg)) //converts to uppercase
-	fmt.Println(strings.ToLower(stg)) //converts to lowercase
+	//fmt.Println(strings.ToUpper(stg)) //converts to uppercase
+	/*
+		Prints HELLO, WORLD!
+	*/
+	//fmt.Println(strings.ToLower(stg)) //converts to lowercase
+	/*
+		Prints hello, world!
+	*/
 }
