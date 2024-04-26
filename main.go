d4281e607932eb8af6aafa2a74d5c7f85a7c76fa
agichimu <alexandergichimu@zetech.ac.ke>
2024-04-26 11:54:36 +0300

    addition of comments

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	// Go Variables
-	var nameOne string = "Alexander "
-	var nameTwo = "Gichimu"
-	var age int = 23
-	x := 4
-	y := 5
-	fmt.Println(x * y)
-	fmt.Println(nameOne, nameTwo, age)
+
+	/*Variables are used to Store data*/
+
+	var nameOne string = "Alexander"
+	var agency string = "fast track!"
+	var totalDevelopers int = 29
+	carBrand := "Porsche."
+	startingPrice := 50.55 /*Float*/
+
+	/*
+		Agency = name of the variable
+		String is a datatype
+	*/
+
+	/*
+	   x := 4
+	   y := 5
+	   fmt.Println(x * y)
+	*/
+
+	fmt.Println("Hello", nameOne)
+	fmt.Println(agency)
+	fmt.Println("My dream car is a", carBrand)
+	fmt.Println("My team consists of", totalDevelopers, "Developers.")
+	fmt.Printf("Our Prices range From %v", startingPrice)
+
+	/*
+		%v holds place of a variable in a formatted string
+	*/
 }
